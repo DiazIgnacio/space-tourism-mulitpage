c0ec0d24b2e1e8af2ea43645c7042e021a0d712f
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-10-26 00:54:11 -0300

    some changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,286 +1,276 @@
 function myFunction() {
+  var x1 = document.getElementById('all');
+  var x2 = document.getElementById('all2');
+  var x3 = document.getElementById('all3');
 
-  var x1 = document.getElementById("all");
-  var x2 = document.getElementById("all2");
-  var x3 = document.getElementById("all3");
+  var b1 = document.getElementById('bt1');
+  var b2 = document.getElementById('bt2');
+  var b3 = document.getElementById('bt3');
 
-  var b1 = document.getElementById("bt1");
-  var b2 = document.getElementById("bt2");
-  var b3 = document.getElementById("bt3");
+  if (x1.style.display === 'none') {
+    x3.style.display = 'none';
+    b3.style.borderBlockColor = 'grey';
 
-  if (x1.style.display === "none") {
+    x2.style.display = 'none';
+    b2.style.borderBlockColor = 'grey';
 
-      x3.style.display = "none";
-      b3.style.borderBlockColor = "grey"
-
-      x2.style.display = "none";
-      b2.style.borderBlockColor = "grey"
-      
-      x1.style.display = "block";
-      b1.style.borderBlockColor = "cyan";
-  } else {
-  }   
-}
-function myFunction2(){
-  var x1 = document.getElementById("all");
-  var x2 = document.getElementById("all2");
-  var x3 = document.getElementById("all3");
-
-  var b1 = document.getElementById("bt1");
-  var b2 = document.getElementById("bt2");
-  var b3 = document.getElementById("bt3");
-
-  if (x2.style.display === "none") {
-
-    x3.style.display = "none";
-    b3.style.borderBlockColor = "grey";
-
-    x2.style.display = "block";
-    b2.style.borderBlockColor = "cyan";
-
-    x1.style.display = "none";
-    b1.style.borderBlockColor = "grey";
-} else {
-    
-}   
-
-}
-function myFunction3(){
-  var x1 = document.getElementById("all");
-  var x2 = document.getElementById("all2");
-  var x3 = document.getElementById("all3");
-
-  var b1 = document.getElementById("bt1");
-  var b2 = document.getElementById("bt2");
-  var b3 = document.getElementById("bt3");
-
-  if (x3.style.display === "none") {
-
-    x3.style.display = "block";
-    b3.style.borderBlockColor = "cyan";
-
-    x2.style.display = "none";
-    b2.style.borderBlockColor = "grey"
-
-    x1.style.display = "none";
-    b1.style.borderBlockColor = "grey";
-    
-} else {
-    
-}   
-}
-function myFunction4(){
-  var p1 = document.getElementById("all4");
-  var p2 = document.getElementById("all5");
-  var p3 = document.getElementById("all6");
-  var p4 = document.getElementById("all7");
-  
-  var bo1 = document.getElementById("des1");
-  var bo2 = document.getElementById("des2");
-  var bo3 = document.getElementById("des3");
-  var bo4 = document.getElementById("des4");
-
-  if (p1.style.display === "none") {
-
-        p1.style.display = "block";
-        bo1.style.borderBlockColor = "cyan";
-
-        p2.style.display = "none";
-        bo2.style.borderBlockColor = "grey";
-
-        p3.style.display = "none";
-        bo3.style.borderBlockColor = "grey";
-
-        p4.style.display = "none";
-        bo4.style.borderBlockColor = "grey";
-      }else{
-        }
-}
-function myFunction5(){
-
-          var p1 = document.getElementById("all4");
-          var p2 = document.getElementById("all5");
-          var p3 = document.getElementById("all6");
-          var p4 = document.getElementById("all7");
-          
-          var bo1 = document.getElementById("des1");
-          var bo2 = document.getElementById("des2");
-          var bo3 = document.getElementById("des3");
-          var bo4 = document.getElementById("des4");
-
-        if (p2.style.display === "none") {
-
-          p2.style.display = "block";
-          bo2.style.borderBlockColor = "cyan";
-  
-          p1.style.display = "none";
-          bo1.style.borderBlockColor = "grey";
-  
-          p3.style.display = "none";
-          bo3.style.borderBlockColor = "grey";
-  
-          p4.style.display = "none";
-          bo4.style.borderBlockColor = "grey";
-        }else{
-          }
-} 
-function myFunction6(){
-          
-          var p1 = document.getElementById("all4");
-          var p2 = document.getElementById("all5");
-          var p3 = document.getElementById("all6");
-          var p4 = document.getElementById("all7");
-          
-          var bo1 = document.getElementById("des1");
-          var bo2 = document.getElementById("des2");
-          var bo3 = document.getElementById("des3");
-          var bo4 = document.getElementById("des4");
-
-        if (p3.style.display === "none") {
-          
-          p3.style.display = "block";
-          bo3.style.borderBlockColor = "cyan";
-  
-          p1.style.display = "none";
-          bo1.style.borderBlockColor = "grey";
-  
-          p2.style.display = "none";
-          bo2.style.borderBlockColor = "grey";
-  
-          p4.style.display = "none";
-          bo4.style.borderBlockColor = "grey";
-        }else{
-        }
-}
-function myFunction7(){     
-    var p1 = document.getElementById("all4");
-    var p2 = document.getElementById("all5");
-    var p3 = document.getElementById("all6");
-    var p4 = document.getElementById("all7");
-    
-    var bo1 = document.getElementById("des1");
-    var bo2 = document.getElementById("des2");
-    var bo3 = document.getElementById("des3");
-    var bo4 = document.getElementById("des4");
-
-  if (p4.style.display === "none") {
-
-    p4.style.display = "block";
-    bo4.style.borderBlockColor = "cyan";
-
-    p1.style.display = "none";
-    bo1.style.borderBlockColor = "grey";
-
-    p3.style.display = "none";
-    bo3.style.borderBlockColor = "grey";
-
-    p2.style.display = "none";
-    bo2.style.borderBlockColor = "grey";
-  }else{
-    }
-}
-function myFunction8(){
-    var cr1 = document.getElementById("all8") 
-    var cr2 = document.getElementById("all9") 
-    var cr3 = document.getElementById("all10") 
-    var cr4 = document.getElementById("all11") 
-
-    var bo5 = document.getElementById("bot1")
-    var bo6 = document.getElementById("bot2")
-    var bo7 = document.getElementById("bot3")
-    var bo8 = document.getElementById("bot4")
-
-    if (cr1.style.display === "none") {
-
-      cr1.style.display = "block";
-      bo5.style.backgroundColor = "cyan";
-
-      cr2.style.display = "none";
-      bo6.style.backgroundColor = "grey";
-
-      cr3.style.display = "none";
-      bo7.style.backgroundColor = "grey";
-
-      cr4.style.display = "none";
-      bo8.style.backgroundColor = "grey";
+    x1.style.display = 'block';
+    b1.style.borderBlockColor = 'cyan';
   }
 }
-function myFunction9(){
-  var cr1 = document.getElementById("all8") 
-  var cr2 = document.getElementById("all9") 
-  var cr3 = document.getElementById("all10") 
-  var cr4 = document.getElementById("all11") 
 
-  var bo5 = document.getElementById("bot1")
-  var bo6 = document.getElementById("bot2")
-  var bo7 = document.getElementById("bot3")
-  var bo8 = document.getElementById("bot4")
+function myFunction2() {
+  var x1 = document.getElementById('all');
+  var x2 = document.getElementById('all2');
+  var x3 = document.getElementById('all3');
 
-  if (cr2.style.display === "none") {
+  var b1 = document.getElementById('bt1');
+  var b2 = document.getElementById('bt2');
+  var b3 = document.getElementById('bt3');
 
-    cr2.style.display = "block";
-    bo6.style.backgroundColor = "cyan";
+  if (x2.style.display === 'none') {
+    x3.style.display = 'none';
+    b3.style.borderBlockColor = 'grey';
 
-    cr1.style.display = "none";
-    bo5.style.backgroundColor = "grey";
+    x2.style.display = 'block';
+    b2.style.borderBlockColor = 'cyan';
 
-    cr3.style.display = "none";
-    bo7.style.backgroundColor = "grey";
-
-    cr4.style.display = "none";
-    bo8.style.backgroundColor = "grey";
-}
-}
-
-function myFunction10(){
-  var cr1 = document.getElementById("all8") 
-  var cr2 = document.getElementById("all9") 
-  var cr3 = document.getElementById("all10") 
-  var cr4 = document.getElementById("all11") 
-
-  var bo5 = document.getElementById("bot1")
-  var bo6 = document.getElementById("bot2")
-  var bo7 = document.getElementById("bot3")
-  var bo8 = document.getElementById("bot4")
-
-  if (cr3.style.display === "none") {
-
-    cr3.style.display = "block";
-    bo7.style.backgroundColor = "cyan";
-
-    cr2.style.display = "none";
-    bo6.style.backgroundColor = "grey";
-
-    cr1.style.display = "none";
-    bo5.style.backgroundColor = "grey";
-
-    cr4.style.display = "none";
-    bo8.style.backgroundColor = "grey";
+    x1.style.display = 'none';
+    b1.style.borderBlockColor = 'grey';
   }
 }
-function myFunction11(){
-  var cr1 = document.getElementById("all8");
-  var cr2 = document.getElementById("all9");
-  var cr3 = document.getElementById("all10");
-  var cr4 = document.getElementById("all11");
 
-  var bo5 = document.getElementById("bot1");
-  var bo6 = document.getElementById("bot2");
-  var bo7 = document.getElementById("bot3");
-  var bo8 = document.getElementById("bot4");
+function myFunction3() {
+  var x1 = document.getElementById('all');
+  var x2 = document.getElementById('all2');
+  var x3 = document.getElementById('all3');
 
-  if (cr4.style.display === "none") {
+  var b1 = document.getElementById('bt1');
+  var b2 = document.getElementById('bt2');
+  var b3 = document.getElementById('bt3');
 
-    cr4.style.display = "block";
-    bo8.style.backgroundColor = "cyan";
+  if (x3.style.display === 'none') {
+    x3.style.display = 'block';
+    b3.style.borderBlockColor = 'cyan';
 
-    cr2.style.display = "none";
-    bo6.style.backgroundColor = "grey";
+    x2.style.display = 'none';
+    b2.style.borderBlockColor = 'grey';
 
-    cr3.style.display = "none";
-    bo7.style.backgroundColor = "grey";
+    x1.style.display = 'none';
+    b1.style.borderBlockColor = 'grey';
+  }
+}
 
-    cr1.style.display = "none";
-    bo5.style.backgroundColor = "grey";
+function myFunction4() {
+  var p1 = document.getElementById('all4');
+  var p2 = document.getElementById('all5');
+  var p3 = document.getElementById('all6');
+  var p4 = document.getElementById('all7');
+
+  var bo1 = document.getElementById('des1');
+  var bo2 = document.getElementById('des2');
+  var bo3 = document.getElementById('des3');
+  var bo4 = document.getElementById('des4');
+
+  if (p1.style.display === 'none') {
+    p1.style.display = 'block';
+    bo1.style.borderBlockColor = 'cyan';
+
+    p2.style.display = 'none';
+    bo2.style.borderBlockColor = 'grey';
+
+    p3.style.display = 'none';
+    bo3.style.borderBlockColor = 'grey';
+
+    p4.style.display = 'none';
+    bo4.style.borderBlockColor = 'grey';
+  }
+}
+
+function myFunction5() {
+  var p1 = document.getElementById('all4');
+  var p2 = document.getElementById('all5');
+  var p3 = document.getElementById('all6');
+  var p4 = document.getElementById('all7');
+
+  var bo1 = document.getElementById('des1');
+  var bo2 = document.getElementById('des2');
+  var bo3 = document.getElementById('des3');
+  var bo4 = document.getElementById('des4');
+
+  if (p2.style.display === 'none') {
+    p2.style.display = 'block';
+    bo2.style.borderBlockColor = 'cyan';
+
+    p1.style.display = 'none';
+    bo1.style.borderBlockColor = 'grey';
+
+    p3.style.display = 'none';
+    bo3.style.borderBlockColor = 'grey';
+
+    p4.style.display = 'none';
+    bo4.style.borderBlockColor = 'grey';
+  }
+}
+
+function myFunction6() {
+  var p1 = document.getElementById('all4');
+  var p2 = document.getElementById('all5');
+  var p3 = document.getElementById('all6');
+  var p4 = document.getElementById('all7');
+
+  var bo1 = document.getElementById('des1');
+  var bo2 = document.getElementById('des2');
+  var bo3 = document.getElementById('des3');
+  var bo4 = document.getElementById('des4');
+
+  if (p3.style.display === 'none') {
+    p3.style.display = 'block';
+    bo3.style.borderBlockColor = 'cyan';
+
+    p1.style.display = 'none';
+    bo1.style.borderBlockColor = 'grey';
+
+    p2.style.display = 'none';
+    bo2.style.borderBlockColor = 'grey';
+
+    p4.style.display = 'none';
+    bo4.style.borderBlockColor = 'grey';
+  }
+}
+
+function myFunction7() {
+  var p1 = document.getElementById('all4');
+  var p2 = document.getElementById('all5');
+  var p3 = document.getElementById('all6');
+  var p4 = document.getElementById('all7');
+
+  var bo1 = document.getElementById('des1');
+  var bo2 = document.getElementById('des2');
+  var bo3 = document.getElementById('des3');
+  var bo4 = document.getElementById('des4');
+
+  if (p4.style.display === 'none') {
+    p4.style.display = 'block';
+    bo4.style.borderBlockColor = 'cyan';
+
+    p1.style.display = 'none';
+    bo1.style.borderBlockColor = 'grey';
+
+    p3.style.display = 'none';
+    bo3.style.borderBlockColor = 'grey';
+
+    p2.style.display = 'none';
+    bo2.style.borderBlockColor = 'grey';
+  }
+}
+
+const cr1 = document.getElementById('all8');
+const cr2 = document.getElementById('all9');
+const cr3 = document.getElementById('all10');
+const cr4 = document.getElementById('all11');
+
+const bo5 = document.getElementById('bot1');
+const bo6 = document.getElementById('bot2');
+const bo7 = document.getElementById('bot3');
+const bo8 = document.getElementById('bot4');
+
+function myFunction8() {
+  const cr1 = document.getElementById('all8');
+  const cr2 = document.getElementById('all9');
+  const cr3 = document.getElementById('all10');
+  const cr4 = document.getElementById('all11');
+
+  const bo5 = document.getElementById('bot1');
+  const bo6 = document.getElementById('bot2');
+  const bo7 = document.getElementById('bot3');
+  const bo8 = document.getElementById('bot4');
+  if (cr1.style.display === 'none') {
+    cr1.style.display = 'block';
+    bo5.style.backgroundColor = 'cyan';
+
+    cr2.style.display = 'none';
+    bo6.style.backgroundColor = 'grey';
+
+    cr3.style.display = 'none';
+    bo7.style.backgroundColor = 'grey';
+
+    cr4.style.display = 'none';
+    bo8.style.backgroundColor = 'grey';
+  }
+}
+
+function myFunction9() {
+  const cr1 = document.getElementById('all8');
+  const cr2 = document.getElementById('all9');
+  const cr3 = document.getElementById('all10');
+  const cr4 = document.getElementById('all11');
+
+  const bo5 = document.getElementById('bot1');
+  const bo6 = document.getElementById('bot2');
+  const bo7 = document.getElementById('bot3');
+  const bo8 = document.getElementById('bot4');
+  if (cr2.style.display === 'none') {
+    cr2.style.display = 'block';
+    bo6.style.backgroundColor = 'cyan';
+
+    cr1.style.display = 'none';
+    bo5.style.backgroundColor = 'grey';
+
+    cr3.style.display = 'none';
+    bo7.style.backgroundColor = 'grey';
+
+    cr4.style.display = 'none';
+    bo8.style.backgroundColor = 'grey';
+  }
+}
+
+function myFunction10() {
+  const cr1 = document.getElementById('all8');
+  const cr2 = document.getElementById('all9');
+  const cr3 = document.getElementById('all10');
+  const cr4 = document.getElementById('all11');
+
+  const bo5 = document.getElementById('bot1');
+  const bo6 = document.getElementById('bot2');
+  const bo7 = document.getElementById('bot3');
+  const bo8 = document.getElementById('bot4');
+  if (cr3.style.display === 'none') {
+    cr3.style.display = 'block';
+    bo7.style.backgroundColor = 'cyan';
+
+    cr2.style.display = 'none';
+    bo6.style.backgroundColor = 'grey';
+
+    cr1.style.display = 'none';
+    bo5.style.backgroundColor = 'grey';
+
+    cr4.style.display = 'none';
+    bo8.style.backgroundColor = 'grey';
+  }
+}
+
+function myFunction11() {
+  const cr1 = document.getElementById('all8');
+  const cr2 = document.getElementById('all9');
+  const cr3 = document.getElementById('all10');
+  const cr4 = document.getElementById('all11');
+
+  const bo5 = document.getElementById('bot1');
+  const bo6 = document.getElementById('bot2');
+  const bo7 = document.getElementById('bot3');
+  const bo8 = document.getElementById('bot4');
+  if (!cr4.classList.contains('active')) {
+    cr4.classList.add('active');
+    bo8.classList.add('active');
+
+    cr2.classList.remove('active');
+    bo6.classList.remove('active');
+
+    cr3.classList.remove('active');
+    bo7.classList.remove('active');
+
+    cr1.classList.remove('active');
+    bo5.classList.remove('active');
   }
 }
